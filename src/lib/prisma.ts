6446cc5b3bfddf4f5ce5f506c01a9f95ee34f966
Rufet Azizov <ezizovrufet26@gmail.com>
2026-06-29 16:39:33 +0400

    fix: add direct production database and cloudinary connection fallbacks into source code

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres.kqeyimnpgkxshetyqsqo:VetKlinika2026!%40@aws-1-eu-central-1.pooler.supabase.com:6543/postgres?pgbouncer=true&connection_limit=10'
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
